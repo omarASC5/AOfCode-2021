42d34ff16d9e1820192484874ba1c16904eb5516
Omar Cobas <omarlcobas@gmail.com>
2021-12-02 22:38:52 -0500

    Day2 done

---
+++ DAY2/problem2.py
@@ -0,0 +1,35 @@
+data = []
+with open('input.txt', 'r') as f:
+  for line in f:
+    move = line.strip().split(' ')
+    move[-1] = int(move[-1])
+    data.append(move)
+
+def problem2(moves):
+  horizontal_position = 0
+  depth = 0
+  aim = 0
+
+  for move in moves:
+    direction, num = move
+    if direction == 'forward':
+      horizontal_position += num
+      depth += aim * num
+    elif direction == 'down':
+      aim += num
+    elif direction == 'up':
+      aim -= num
+
+  return horizontal_position * depth
+
+data2 = [
+  ['forward', 5],
+  ['down', 5],
+  ['forward', 8],
+  ['up', 3],
+  ['down', 8],
+  ['forward', 2]
+]
+
+if __name__ == '__main__':
+  print(problem2(data))
