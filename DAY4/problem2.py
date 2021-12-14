1e4ab48755722b2134c0996d9ab619171b667a19
omarASC5 <ocobas19@avenues.org>
2021-12-13 20:36:31 -0500

    finished day4 part2

--- DAY4/problem2.py
+++ DAY4/problem2.py
@@ -30,8 +30,13 @@ with open('input.txt', 'r') as f:
 
   bingo_boards.append(bingo_board)
 
-def mark_position(bingo_board, current_bingo_board, number):
+def mark_position(index, bingo_board, current_bingo_board, number, completed, solutions):
+  if all(completed): return
+  # Checks entire (single) bingo board
+  # width of the board
   for i in range(len(bingo_board)):
+
+    # height of the board
     for j in range(len(bingo_board[i])):
       if bingo_board[i][j] == number:
         current_bingo_board[i][j] = True
@@ -41,20 +46,19 @@ def mark_position(bingo_board, current_bingo_board, number):
           column.append(current_bingo_board[k][j])
 
         if all(current_bingo_board[i]) or all(column):
-          return number, bingo_board, current_bingo_board
+          solutions.append((number, bingo_board, current_bingo_board))
+          completed[index] = True
 
-def mark_positions(bingo_boards, current_bingo_boards, number):
+def mark_positions(bingo_boards, current_bingo_boards, number, completed, solutions):
+  if all(completed): return
   for i, bingo_board in enumerate(bingo_boards):
     current_bingo_board = current_bingo_boards[i]
-    marked_data = mark_position(bingo_board, current_bingo_board, number)
-    if marked_data:
-      return marked_data
+    mark_position(i, bingo_board, current_bingo_board, number, completed, solutions)
 
-def mark_all_positions(bingo_boards, current_bingo_boards, numbers):
+def mark_all_positions(bingo_boards, current_bingo_boards, numbers, completed, solutions):
+  if all(completed): return
   for number in numbers:
-    marked_data = mark_positions(bingo_boards, current_bingo_boards, number)
-    if marked_data:
-      return marked_data
+    mark_positions(bingo_boards, current_bingo_boards, number, completed, solutions)
 
 def sum_unmarked_positions(bingo_board, marked_bingo_board):
   result = 0
@@ -67,8 +71,13 @@ def sum_unmarked_positions(bingo_board, marked_bingo_board):
   return result
 
 def get_final_score(bingo_boards, current_bingo_boards, numbers):
-  marked_number, winning_bingo_board, winning_marked_bingo_board = mark_all_positions(bingo_boards, current_bingo_boards, numbers)
+  completed = [False] * len(bingo_boards)
+  solutions = []
+  
+  mark_all_positions(bingo_boards, current_bingo_boards, numbers, completed, solutions)
+  marked_number, winning_bingo_board, winning_marked_bingo_board = solutions[-1]
   unmarked_sum = sum_unmarked_positions(winning_bingo_board, winning_marked_bingo_board)
+
   return marked_number * unmarked_sum
 
 if __name__ == '__main__':
@@ -82,5 +91,6 @@ if __name__ == '__main__':
         )
       .to_list()
   )
+
   print(get_final_score(bingo_boards, current_bingo_boards, numbers))
   
